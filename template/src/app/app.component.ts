ebbc3cfddcc59d7e889158a9e0485af92763a7fa
Allen Kim <allenhwkim@gmail.com>
2019-02-11 13:23:20 -0500

    Update app.component.ts

--- template/src/app/app.component.ts
+++ template/src/app/app.component.ts
@@ -2,9 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  templateUrl: `
+    <div class="container-fluid">
+      <div class="jumbotron">
+        <h1>Hello Angular 7</h1>
+        My name is {{name}}.
+      </div>
+      <oneview-header></oneview-header>
+    </div>`,
+  styles: [`
+    p {
+      font-family: Lato;
+    }`]
 })
 export class AppComponent  {
-  name = 'Angular';
+  name = 'Allen Kim';
 }
